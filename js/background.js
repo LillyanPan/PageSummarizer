bd8e1373fdce4f0d1338bf324eac1b00fcf58ed9
Lillyan <ldp54@cornell.edu>
2016-04-01 16:50:30 -0400

    initial commit

---
+++ js/background.js
@@ -0,0 +1,5 @@
+chrome.browserAction.onClicked.addListener(function(tab) {
+	chrome.tabs.executeScript( {file: "d3/d3.min.js"});
+	// chrome.tabs.executeScript( {file: "js/firebase.js"});
+	chrome.tabs.executeScript( {file: "popup.js"});
+});
